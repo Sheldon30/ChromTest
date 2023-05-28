30ec0082c85c13b4691be7a01eef140cc831a841
Sheldon <aneln700@gmail.com>
2023-05-28 20:40:18 +0300

    Initial

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class SeleniumTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+    private WebDriver driver;
+    @BeforeAll
+    static void setUpAll(){
+       WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+    }
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    void shouldOpenSite() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        Thread.sleep(50000);
+
+
+
+
+
+
+
+    }
+
 }
