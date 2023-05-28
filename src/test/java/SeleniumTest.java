5eaf8ffc874019782bdf5e06877153203d7c4ab3
Sheldon <aneln700@gmail.com>
2023-05-28 22:00:35 +0300

    Test

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -3,9 +3,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SeleniumTest {
     private WebDriver driver;
@@ -30,7 +33,15 @@ public class SeleniumTest {
     @Test
     void shouldOpenSite() throws InterruptedException {
         driver.get("http://localhost:9999/");
-        Thread.sleep(50000);
+        driver.findElement(By.cssSelector("[data-test-id='name']")).sendKeys("Александр");
+        driver.findElement(By.cssSelector("[data-test-id='phone']")).sendKeys("+79275896556");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        assertEquals(expected,actual);
+
+
 
 
 
